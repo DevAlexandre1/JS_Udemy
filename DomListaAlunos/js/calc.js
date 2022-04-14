f3ad966008c079ac07952cb6e6f0d4e9f460a829
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-04-13 23:08:44 -0300

    Dinamicamente DomArray

--- DomListaAlunos/js/calc.js
+++ DomListaAlunos/js/calc.js
@@ -1,5 +1,4 @@
 function sum() {
-    // const numbers = Array.from(arguments)
     const numbers = [...arguments]
     return numbers.reduce(function (sum, atual) {
         return sum + atual
@@ -8,3 +7,42 @@ function sum() {
 function avarege() {
     return sum(...arguments) / arguments.length
 }
+
+
+(function(){
+    function pegaIndice(indice){
+      const trHeader = document.querySelector("thead tr")
+      const thsHeader = trHeader.querySelectorAll("th")
+
+      const th = trHeader.querySelector(`[aluno-nota="${indice}"]`)
+      
+      const i = [...thsHeader].indexOf(th)
+      return i
+    }
+
+    const INDICE_NOTAS = {
+        n1: pegaIndice("n1"),
+        n2: pegaIndice("n2"),
+        n3: pegaIndice("n3"),
+        n4: pegaIndice("n4"),
+        media: pegaIndice("media")
+    }
+   
+    const trs = document.querySelectorAll("tbody tr")   
+    let x = 0
+    let media = 0
+
+    while (trs[x]){
+        const tds = trs[x].querySelectorAll("td")
+        
+        media =  avarege(
+            parseFloat(tds[INDICE_NOTAS.n1].textContent),
+            parseFloat(tds[INDICE_NOTAS.n2].textContent),
+            parseFloat(tds[INDICE_NOTAS.n3].textContent),
+            parseFloat(tds[INDICE_NOTAS.n4].textContent)
+
+        )
+        x++
+        tds[INDICE_NOTAS.media].textContent = media
+    }
+})()
