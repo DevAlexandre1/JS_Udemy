d5915144284a85d43ebd468bbe7d456f1a35be49
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-04-27 17:20:11 -0300

    Treinando arrays

--- Array/treinosArray.js
+++ Array/treinosArray.js
@@ -28,9 +28,10 @@ const numerosPar = []
 const numerosImpar = []
 
 
-arrayMain.forEach((valor)=>{
+arrayMain.forEach((valor,indice,array)=>{
+    //o indice e o array são opcionais
     //console.log(`O index é ${index} e o valor do elemento é ${valor}`)
-
+    
     if(valor % 2 ===0){
         numerosPar.push(valor)
     }else{
@@ -38,5 +39,33 @@ arrayMain.forEach((valor)=>{
     }
 })
 
-console.log(numerosImpar)
-console.log(numerosPar)
+//console.log(numerosImpar)
+//console.log(numerosPar)
+
+//Multiplique os valores abaixo em 3x
+const arrayMain1 = [1,3,5,2,6,12,34,23,4,5,89,76]
+
+
+const arrayMult = arrayMain1.map((element)=>{
+    return element * 3
+})
+//console.log(arrayMult)
+
+
+
+//
+function formatarNome(nomeCompleto){
+    nomeCompleto = nomeCompleto.trim()
+    let primeiroEspaco =  nomeCompleto.indexOf(" ")
+    if(primeiroEspaco <0){
+        return nomeCompleto
+    }
+    
+    let primeiroNome = nomeCompleto.slice(0,primeiroEspaco)
+    let sobreNome = nomeCompleto.slice(primeiroEspaco+1)
+    return sobreNome +", "+ primeiroNome
+}
+
+// console.log(formatarNome("Daniel")) //retornar Daniel    
+// console.log(formatarNome("Daniel Moraes")) //retornar Morais, Daniel    
+// console.log(formatarNome("Daniel Tapilas Moraes")) //retornar Tapilas Morales,Daniel    
