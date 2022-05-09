3378c7d2a0cba6a114112c093b729bd395fb7ac1
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-05-08 22:25:33 -0300

    Calculadora finalizada

--- Treino/Calculadora/calc.js
+++ Treino/Calculadora/calc.js
@@ -1,11 +1,20 @@
 const painel = document.querySelector("#painel")
 
 const btnNum = document.querySelectorAll(".btnEntrada")
+const btnOpe = document.querySelectorAll(".btnOperadorEntrada")
 
-const btnOperadores = document.querySelectorAll(".btnOperadorEntrada")
+
 
 let valorPainel = []
 let valorShow = "" 
+let valor1 = 0
+let TamanhoArray = 0
+let opereradorCalc = 0
+let arr1 = []
+let arr2 = []
+
+
+
 
 // Botoes de entrada
 btnNum[0].addEventListener("click", function addValores(){
@@ -63,11 +72,70 @@ btnNum[10].addEventListener("click", function addValores(){
     valorShow = valorPainel.join("")
     painel.textContent = valorShow
 })
-//botao =
+
 btnNum[11].addEventListener("click", function addValores(){
-    valorPainel.push(btnNum[11].value)
-    valorShow = valorPainel.join("")
+    TamanhoArray = valorPainel.length   
+    opereradorCalc = valorPainel[valor1]
+
+    valor2 = valor1 +  1
+  
+    arr1 = valorPainel.slice(0,valor1)
+    arr2 = valorPainel.slice(valor2,TamanhoArray)
+
+    valor1num = arr1.join("")
+    valor2num = arr2.join("")
+
+    switch (opereradorCalc) {
+ case "+":
+    valorShow =  parseFloat(valor1num) + parseFloat(valor2num)
     painel.textContent = valorShow
+ break;
+
+ case "-":
+    valorShow =  parseFloat(valor1num) - parseFloat(valor2num)
+    painel.textContent = valorShow
+ break;
+
+ case "*":
+    valorShow =  parseFloat(valor1num) * parseFloat(valor2num)
+    painel.textContent = valorShow
+ break;
+
+ case "/":
+    valorShow =  parseFloat(valor1num) / parseFloat(valor2num)
+    painel.textContent = valorShow
+ break;
+    }
+
 })
 
+
 //botões operadores
+btnOpe[0].addEventListener("click", function addOperadores(){
+    valor1 = valorPainel.length
+    valorPainel.push(btnOpe[0].value)
+    valorShow = valorPainel.join("")
+    painel.textContent = valorShow
+    
+} )
+btnOpe[1].addEventListener("click", function addOperadores(){
+    valor1 = valorPainel.length
+    valorPainel.push(btnOpe[1].value)
+    valorShow = valorPainel.join("")
+    painel.textContent = valorShow
+    
+} )
+btnOpe[2].addEventListener("click", function addOperadores(){
+    valor1 = valorPainel.length
+    valorPainel.push(btnOpe[2].value)
+    valorShow = valorPainel.join("")
+    painel.textContent = valorShow
+    
+} )
+btnOpe[3].addEventListener("click", function addOperadores(){
+    valor1 = valorPainel.length
+    valorPainel.push(btnOpe[3].value)
+    valorShow = valorPainel.join("")
+    painel.textContent = valorShow
+    
+} )
