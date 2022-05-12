0eff6d9bfc25f48c8854fae0d9db706877a35c56
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-05-12 17:47:18 -0300

    Object create

--- Objetos_II/gets-sets/DesafioPessoa/DesafioPessoa.js
+++ Objetos_II/gets-sets/DesafioPessoa/DesafioPessoa.js
@@ -1,30 +1,26 @@
-let _usuarios=[]
-let user = ""
-let consultaArr = ""
-const pessoa = {
-    get usuarios(){
-        return _usuarios
-    },
-    set addUser(user){        
-            _usuarios.forEach(function(el){
-                if(el == user){
-                    console.log("Usuário ja cadastrado")
-                }//corrigir a função abaixo
-                _usuarios.push(user)
-                console.log("Usuário cadastrado com sucesso")            
-            })        
-        }
-    },
-    get usuario(){
-        let findUser = _usuarios.length
-        return _usuarios[findUser]
+(function(){
+    let _usuarios=[]
+    this.pessoa = {
+        
+        get usuario(){
+            if(_usuarios.length){
+                return _usuarios[_usuarios.length -1]
+
+            }
+        },
+        set addUser(user){        
+                if(_usuarios.indexOf(user)< 0){
+                    _usuarios.push(user)
+                }else{
+                    console.log("Usuário já consta na listagem")
+                }
+        },  
+        get usuarios(){
+            //Forma de proteger as referencias do seu codigo, inserir este metodo nos seus gets
+            //Este metodo cria uma copia do retorno e impede que alterações utizando referencia funcione
+            return [].concat(_usuarios)
+        },
     }
+})()
 
-}
 
-//pessoa.addUser =  "Marcos"
-console.log(_usuarios)
-pessoa.usuarios
-// console.log(consultaArr)
-// console.log(typeof _usuarios)
-// console.log(typeof consultaArr)
